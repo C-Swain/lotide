38c886399393de93d6bbfcf592297684a2b3409b
C-Swain <catherinemexicoswain@gmail.com>
2021-09-20 09:43:07 +0000

    now with 300% the emojiis

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,13 @@
+
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    console.log(`😸 Assertion Passed: ${actual} ===  ${expected}`); }
-  else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected} `);
+    console.log(`😸😸😸 Assertion Passed: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+ module.exports = assertEqual;
+
