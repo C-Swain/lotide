f0e113464c35c6c9446e3e74be6f2e2dd52580d1
C-Swain <catherinemexicoswain@gmail.com>
2021-09-20 09:39:31 +0000

    moved to test folder

--- head.js
+++ head.js
@@ -1,20 +1,8 @@
 
-// program that can be used to verify if the answer is expected 
-const assertEqual = function(actual, expected) {
 
-  if (actual === expected) {
-    console.log(`😸 Assertion Passed: ${actual} ===  ${expected}`); }
-  else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected} `);
-  }
-};
-
-// program that returns the head if the array 
+// program that returns the head if the array
 const head = function(arr) {
   return arr[0];
 };
 
-
-
-// test question 
-assertEqual(head([5,6,7]), 5);
+module.exports = head;
