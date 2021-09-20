bbfb09aa3578600b6f7f0703e48cf062e3c49bf5
C-Swain <catherinemexicoswain@gmail.com>
2021-09-20 10:47:56 +0000

    new and improved

--- middle.js
+++ middle.js
@@ -1,42 +1,23 @@
-const eqArrays = function (actual, expected){
-  let match = true
-  for(let i =0; i < actual.length; i ++){
-  if (actual[i] !== expected[i]) {
-    match = false   
-  }  
-} return match
-};
-
-const assertArrayEqual = function(actual, expected) {
-  let equivalentArrays = eqArrays(actual, expected);
-  if (equivalentArrays === true) {
-    console.log(`😸Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
-  }
-}; 
 
 const middle = function(array) {
   //first we need to find out if the length of the array is even or odd
 
-let center  = array.length/2;
+  let center  = array.length / 2;
  
-if (array.length % 2 === 0) {
-   var middleOne = center - 1;
-   var middleTwo = center;
-   var newArray = [array[middleOne],array[middleTwo]];
+  if (array.length % 2 === 0) {
+    let middleOne = center - 1;
+    let middleTwo = center;
+    let newArray = [array[middleOne],array[middleTwo]];
 
-  console.log(newArray);
+    return newArray;
 
-// if the number is even we will need to return the two middle indexs
-}
-else { 
+    // if the number is even we will need to return the two middle indexs
+  } else {
 
-  var middleOne = center - 0.5;
-  newArray = [array[middleOne]];
-  console.log(newArray);
- 
-}
-}
+    let middleOne = center - 0.5;
+    let newArray = [array[middleOne]];
+    return newArray;
+  }
+};
 
-middle([1, 2, 7, 5, 9])
+module.exports = middle;
