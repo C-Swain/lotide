82661d90cff949a1d5a5e65c206e3cbfde045fab
C-Swain <catherinemexicoswain@gmail.com>
2021-09-21 13:30:28 +0000

    final updates

--- takeUntil.js
+++ takeUntil.js
@@ -1,23 +1,4 @@
 
-const eqArrays = function (actual, expected){
-  let match = true
-  for(let i =0; i < actual.length; i ++){
-  if (actual[i] !== expected[i]) {
-    match = false   
-  }  
-} return match
-};
-
-
-const assertArraysEqual = function(actual, expected) {
-  let equivalentArrays = eqArrays(actual, expected);
-  if (equivalentArrays === true) {
-    console.log(`😸Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
-  }
-}; 
-
 
 
 const takeUntil = function(array, callback) {
@@ -37,16 +18,4 @@ const takeUntil = function(array, callback) {
   return results;
 };
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
-assertArraysEqual(results1,[1, 2, 5, 7, 2]);
-
-assertArraysEqual(results2,[ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+module.exports = takeUntil;
--- without.js
+++ without.js
@@ -1,22 +1,4 @@
 
-const eqArrays = function (actual, expected){
-  let match = true
-  for(let i =0; i < actual.length; i ++){
-  if (actual[i] !== expected[i]) {
-    match = false   
-  }  
-} return match
-};
-
-
-const assertArrayEqual = function(actual, expected) {
-  let equivalentArrays = eqArrays(actual, expected);
-  if (equivalentArrays === true) {
-    console.log(`😸Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
-  }
-}; 
 
 const without = function(source, unwanted) {
   let newArr = source;
@@ -30,4 +12,4 @@ const without = function(source, unwanted) {
   return newArr;
 };
 
-console.log(without([1, 2, 3], [3]));
+module.exports = without;
